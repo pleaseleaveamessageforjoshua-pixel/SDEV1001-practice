4432175ba083610f5c39554341de53274fe217c3
pleaseleaveamessageforjoshua-pixel <jharvey15@nait.ca>
2026-09-13 21:41:35 -0600

    Added exercise7 - string formatting

---
+++ exercise7.js
@@ -0,0 +1,5 @@
+let city = "Edmonton";
+let country = "Canada";
+let year = 2026;
+let message = `I live in ${city}, which is in ${country}, and the current year is ${year}.`;
+console.log(message);
