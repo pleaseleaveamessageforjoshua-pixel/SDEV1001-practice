063fca6fd960e6c2062b36fa1fdf6629a24f6f4a
pleaseleaveamessageforjoshua-pixel <jharvey15@nait.ca>
2026-09-13 21:37:17 -0600

    Added exercise6-three number math

---
+++ exercise6.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+let sum = num1 + num2 + num3;
+let average = sum / 3;
+let product = num1 * num2 * num3;
+
+console.log(`The sum of ${num1}, ${num2}, and ${num3} is ${sum}.`);
+console.log(`The average of ${num1}, ${num2}, and ${num3} is ${average}.`);
+console.log(`The product of ${num1}, ${num2}, and ${num3} is ${product}.`);
