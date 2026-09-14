482afcfb5cad15b337ea777663d4f476e8a58de7
pleaseleaveamessageforjoshua-pixel <jharvey15@nait.ca>
2026-09-13 21:31:26 -0600

    Added exercise5 - favorite things

---
+++ exercise5.js
@@ -0,0 +1,7 @@
+let favoriteFood = "Salmon";
+let favoriteMovie = "The Matrix";
+let favoriteHobby = "unified field theory";
+
+let message = `My favorite food is ${favoriteFood}, my favorite movie is ${favoriteMovie}, and my favorite hobby is ${favoriteHobby}.`;
+
+console.log(message);
