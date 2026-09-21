687f93b99121a80c1fde65a45bdf145811259590
pleaseleaveamessageforjoshua-pixel <jharvey15@nait.ca>
2026-09-20 20:29:21 -0600

    Add Chapter 5 conditions exercise

---
+++ chapter5_conditions.py
@@ -0,0 +1,19 @@
+# Chapter 5: Checking Multiple Conditions with 'and'
+
+age_0 = 22
+age_1 = 18
+
+# Both conditions must be true for 'and' to evaluate to true
+print("Test 1 (age_1 = 18):", (age_0>= 21) and (age_1 >= 21))
+
+# Updating age_1 to 22
+age_1 = 22
+print("Test 2 (age_1 = 22):", (age_0 >= 21) and (age_1 >= 21))
+
+# Updating age_1 to 22
+age_1 = 22
+print("Test 2 (age_1 = 22):", (age_0 >= 21) and (age_1 >= 21))
+
+#Updating age_1 to 22
+age_1 = 22
+print("Test 2 (age_1 =22):", (age_0 >= 21) and (age_1 >= 21) )
